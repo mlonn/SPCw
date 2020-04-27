0ae724da225b05b8d2a5580a40690f4570d729e8
Mikael Lönn <mikael.lonn@chorus.se>
2020-04-27 19:35:45 +0200

    responsive profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,6 @@
-import { Box, Button, Form, FormField, Heading, RadioButtonGroup, TextInput } from "grommet";
+import { Box, Button, Form, FormField, Heading, RadioButtonGroup, TextInput, Grid, ResponsiveContext } from "grommet";
 import { Clear } from "grommet-icons";
-import React from "react";
+import React, { useContext } from "react";
 import DurationFormField from "../components/form/duration/DurationFormField";
 import PowerFormField from "../components/form/power/PowerFormField";
 import WeightFormField from "../components/form/weight/WeightFormField";
@@ -16,7 +16,7 @@ interface Props {}
 const Profile = (props: Props) => {
   const { weight, tte, ftp, name, gender, powerMeter, units } = useAthleteState();
   const dispatch = useAthleteAction();
-
+  const size = useContext(ResponsiveContext);
   const setWeight = (newWeight: Weight) => {
     dispatch({ type: TypeKeys.SET_WEIGHT, weight: newWeight });
   };
@@ -36,77 +36,85 @@ const Profile = (props: Props) => {
   return (
     <Box alignSelf="center" width="xlarge">
       <Heading level="2">Profile</Heading>
+
       <Form
         onReset={() => {
           dispatch({ type: TypeKeys.CLEAR_PROFILE });
         }}
       >
-        <FormField label="Athelete Name" name="name">
-          <TextInput
-            name="name"
-            value={name || ""}
-            onChange={(e) => dispatch({ type: TypeKeys.SET_NAME, name: e.target.value })}
-          />
-        </FormField>
-        <WeightFormField weight={weight} setWeight={setWeight} />
-        <PowerFormField power={ftp} setPower={setFtp} weight={weight} valueLabel={"FTP/CP"} />
-        <DurationFormField duration={tte} setDuration={setTte} valueLabel={"Time To Exhaustion"} />
-        <Form onReset={() => dispatch({ type: TypeKeys.CLEAR_GENDER })}>
-          <Box direction="row" align="center">
-            <Box fill>
-              <FormField label="Gender">
-                <RadioButtonGroup
-                  direction="row"
-                  name="gender"
-                  value={gender}
-                  options={[...Object.values(Gender)]}
-                  onChange={(e) => dispatch({ type: TypeKeys.SET_GENDER, gender: e.target.value as Gender })}
-                />
-              </FormField>
-            </Box>
-            <Button type="reset" icon={<Clear />} />
+        <Grid columns={size !== "small" ? ["1fr", "1fr"] : undefined} gap="medium">
+          <Box>
+            <Heading level="3">Athlete info</Heading>
+            <FormField label="Athelete Name" name="name">
+              <TextInput
+                name="name"
+                value={name || ""}
+                onChange={(e) => dispatch({ type: TypeKeys.SET_NAME, name: e.target.value })}
+              />
+            </FormField>
+            <WeightFormField weight={weight} setWeight={setWeight} />
+            <PowerFormField power={ftp} setPower={setFtp} weight={weight} valueLabel={"FTP/CP"} />
+            <DurationFormField duration={tte} setDuration={setTte} valueLabel={"Time To Exhaustion"} />
+            <Form onReset={() => dispatch({ type: TypeKeys.CLEAR_GENDER })}>
+              <Box direction="row" align="center">
+                <Box fill>
+                  <FormField label="Gender">
+                    <RadioButtonGroup
+                      direction="row"
+                      name="gender"
+                      value={gender}
+                      options={[...Object.values(Gender)]}
+                      onChange={(e) => dispatch({ type: TypeKeys.SET_GENDER, gender: e.target.value as Gender })}
+                    />
+                  </FormField>
+                </Box>
+                <Button type="reset" icon={<Clear />} />
+              </Box>
+            </Form>
+            <Form onReset={() => dispatch({ type: TypeKeys.CLEAR_POWER_METER })}>
+              <Box direction="row" align="center">
+                <Box fill>
+                  <FormField label="Power meter">
+                    <RadioButtonGroup
+                      direction="row"
+                      name="powermeter"
+                      value={powerMeter}
+                      options={[...Object.values(PowerMeter)]}
+                      onChange={(e) =>
+                        dispatch({ type: TypeKeys.SET_POWER_METER, powerMeter: e.target.value as PowerMeter })
+                      }
+                    />
+                  </FormField>
+                </Box>
+                <Button icon={<Clear />} type="reset" />
+              </Box>
+            </Form>
           </Box>
-        </Form>
-        <Form onReset={() => dispatch({ type: TypeKeys.CLEAR_POWER_METER })}>
-          <Box direction="row" align="center">
-            <Box fill>
-              <FormField label="Power meter">
-                <RadioButtonGroup
-                  direction="row"
-                  name="powermeter"
-                  value={powerMeter}
-                  options={[...Object.values(PowerMeter)]}
-                  onChange={(e) =>
-                    dispatch({ type: TypeKeys.SET_POWER_METER, powerMeter: e.target.value as PowerMeter })
-                  }
-                />
-              </FormField>
-            </Box>
-            <Button icon={<Clear />} type="reset" />
+          <Box>
+            <Heading level="3">Standard Units</Heading>
+            <WeightUnitFormField
+              weight={{ unit: units?.weight }}
+              setWeight={(newWeight) => {
+                setUnits({ ...units, weight: newWeight?.unit });
+              }}
+            />
+            <PowerUnitFormField
+              power={{ unit: units?.power }}
+              setPower={(newPower) => {
+                setUnits({ ...units, power: newPower?.unit });
+              }}
+              weight={weight}
+            />
+            <DurationUnitFormField
+              duration={{ unit: units?.duration }}
+              unitLabel="Duration unit"
+              setDuration={(newDuration) => {
+                setUnits({ ...units, duration: newDuration?.unit });
+              }}
+            />
           </Box>
-          <Heading level="3">Standard Units</Heading>
-          <WeightUnitFormField
-            weight={{ unit: units?.weight }}
-            setWeight={(newWeight) => {
-              setUnits({ ...units, weight: newWeight?.unit });
-            }}
-          />
-          <PowerUnitFormField
-            power={{ unit: units?.power }}
-            setPower={(newPower) => {
-              setUnits({ ...units, power: newPower?.unit });
-            }}
-            weight={weight}
-          />
-          <DurationUnitFormField
-            duration={{ unit: units?.duration }}
-            unitLabel="Duration unit"
-            setDuration={(newDuration) => {
-              setUnits({ ...units, duration: newDuration?.unit });
-            }}
-          />
-          <Button label="Clear profile" type="reset" />
-        </Form>
+        </Grid>
+        <Button label="Clear profile" type="reset" />
       </Form>
     </Box>
   );
