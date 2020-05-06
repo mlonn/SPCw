08e3f6e50b8e59c6065504a652fc7ca2d095acb5
Mikael Lönn <mikael.lonn@chorus.se>
2020-05-06 11:37:43 +0200

    add update to service workers

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,43 @@ import { IdentityContextProvider } from "react-netlify-identity";
 import { Normalize } from "styled-normalize";
 import App from "./App";
 import AthleteProvider from "./context/AthleteProvider";
-import * as serviceWorker from "./serviceWorker";
+import { ServiceWorkerProvider } from "./hooks/useServiceWorker";
 import { theme } from "./theme";
+window.addEventListener("error", async (err) => {
+  // Since we are handling the error here, we must make
+  // sure we log it into the console nonetheless, otherwise
+  // it will be very difficult to understand why your app
+  // is crashing.
+  console.error(err);
 
+  // If no service worker is available, our work ends here
+  // because we don't need to unregister the service worker
+  // to make sure the user is able to get a newer version of
+  // our application.
+  if (!navigator.serviceWorker) {
+    return;
+  }
+
+  // On development builds of React, error boundaries don't stop
+  // errors from bubbling up to the window error handler, so we don't
+  // want to execute this code here because it would be unreliable
+  // https://github.com/facebook/react/issues/12897#issuecomment-410036991
+  if (process.env.NODE_ENV !== "development") {
+    // We want to run this code only if we detect a new service worker
+    // is getting installed or is installed but waiting to be activated.
+    // This will make sure we don't run this code on a sane environment
+    // that is crashing for an error not related to stale app cache.
+    const registration = await navigator.serviceWorker.ready;
+    if (registration.installing || registration.waiting) {
+      navigator.serviceWorker.ready.then(async (reg) => {
+        await reg.unregister();
+        // Once the service worker is unregistered, we can reload
+        // the page to let the browser download a fresh copy of our app
+        window.location.reload();
+      });
+    }
+  }
+});
 Sentry.init({
   dsn: "https://3a9e9da734b046f0b17b868e6cf6e2be@o385775.ingest.sentry.io/5219025",
   release: "spcw@" + process.env.npm_package_version,
@@ -36,21 +70,19 @@ if (state) {
   });
 }
 const url = "https://superpowercalculator.com";
+
 ReactDOM.render(
   <React.StrictMode>
     <Grommet theme={deepMerge(grommet, theme)}>
       <IdentityContextProvider url={url}>
-        <AthleteProvider>
-          <Normalize />
-          <App />
-        </AthleteProvider>
+        <ServiceWorkerProvider>
+          <AthleteProvider>
+            <Normalize />
+            <App />
+          </AthleteProvider>
+        </ServiceWorkerProvider>
       </IdentityContextProvider>
     </Grommet>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
